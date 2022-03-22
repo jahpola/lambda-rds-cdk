89ce2162419ed94868e7585ac749b6135c3eccd5
Jyrki Ahpola <jahpola@users.noreply.github.com>
2022-03-22 12:44:24 +0200

    Basic test upgraded

--- test/lambda-rds.test.ts
+++ test/lambda-rds.test.ts
@@ -1,11 +1,12 @@
-import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as LambdaRds from '../lib/infra-stack';
 
 test('RDS exists', () =>{
     const app = new cdk.App();
 
     const stack = new LambdaRds.LambdaRdsStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
 
-    expectCDK(stack).to(haveResource("AWS::RDS::DBCluster"))
+    template.resourceCountIs("AWS::RDS::DBCluster",1);
 });
