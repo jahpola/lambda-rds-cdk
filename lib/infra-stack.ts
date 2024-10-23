d13f14508165f4764d1f79f40f48ec0fbff8e28d
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-10-23 09:51:32 +0300

    CDK upgrades

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -2,7 +2,7 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from 'path';
 import { Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
-import * as ec2 from 'aws-cdk-lib/aws-ec2'; 
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import { Construct } from "constructs";
 import {
@@ -18,20 +18,19 @@ export class LambdaRdsStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    
+
     // VPC subnet types are
     // ISOLATED: Isolated Subnets do not route traffic to the Internet (in this VPC).
-    // PRIVATE.: Subnet that routes to the internet, but not vice versa.
+    // PRIVATE.: Subnet that routes to the internet using nat gateway(s), but not vice versa.
     // PUBLIC..: Subnet connected to the Internet
     const vpc = new Vpc(this, 'network', {
-      cidr: '10.0.0.0/16',
-      natGateways:1, // In production this should be 3, remember AZ design in AWS ie design for loss of 1 az always
+      natGateways: 1, // In production this should be 3, remember AZ design in AWS ie design for loss of 1 az always
       subnetConfiguration: [
         {
           name: 'public', subnetType: SubnetType.PUBLIC,
         },
         {
-          name: 'private', subnetType: SubnetType.PRIVATE_WITH_NAT
+          name: 'private', subnetType: SubnetType.PRIVATE_WITH_EGRESS
         },
         {
           name: 'db-net', subnetType: SubnetType.PRIVATE_ISOLATED
@@ -46,7 +45,7 @@ export class LambdaRdsStack extends Stack {
     const db_subnet = new rds.SubnetGroup(this, 'db-subnet', {
       description: 'Isolated DB net',
       vpc: vpc,
-      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED}
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED }
     });
 
     const db = new rds.ServerlessCluster(this, 'db', {
@@ -61,14 +60,14 @@ export class LambdaRdsStack extends Stack {
       credentials: rds.Credentials.fromGeneratedSecret("serverless"),
       deletionProtection: false,
       removalPolicy: RemovalPolicy.DESTROY, // Just delete the db, this is hazard!
-      // default ie no subnetgroup = private_with_nat automatically selected
+      // default ie no subnetgroup = private_with_egress automatically selected
       // note that changing these values will destroy the old db and create new one
       subnetGroup: db_subnet
     });
 
     // Yes, the lambda code is utter crap... should be nuked from orbit and rewritten 
     const reader = new NodejsFunction(this, 'reader-function', {
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'main',
       entry: path.join(__dirname, '../src/db-reader/main.ts'),
       tracing: lambda.Tracing.ACTIVE,
@@ -77,28 +76,29 @@ export class LambdaRdsStack extends Stack {
       },
       environment: {
         HOST: db.clusterEndpoint.hostname,
-        DBNAME: db.secret?.secretValueFromJson('dbname').toString()!,
-        USER: db.secret?.secretValueFromJson('username').toString()!,
-        SECRET: db.secret?.secretValueFromJson('password').toString()!
+        // unsafeUnwrap is used to get the value from the secret, not recommended in production
+        DBNAME: db.secret?.secretValueFromJson('dbname').unsafeUnwrap()!,
+        USER: db.secret?.secretValueFromJson('username').unsafeUnwrap()!,
+        SECRET: db.secret?.secretValueFromJson('password').unsafeUnwrap()!
       },
       vpc: vpc,
       vpcSubnets: {
-// PRIVATE_WITH_NAT is required with outside access usually
-// ISOLATTON might require private link+api etc endpoints, 
-// recommendation: just use private_with_nat unless working in high security environments
-        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT 
+        // PRIVATE_WITH_EGRESS is required with outside access usually
+        // ISOLATION might require private link+api etc endpoints, 
+        // recommendation: just use private_with_egress unless working in high security environments
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
       },
       architecture: lambda.Architecture.ARM_64,
     });
 
     // Allow access from reader lambda without creating a sg..
     db.connections.allowDefaultPortFrom(reader)
-    
+
     // Allows DataAPI access
     //db.grantDataApiAccess(reader)
 
     // Outputs for import/export across stacks as needed below
-    new  CfnOutput(this, 'VPCid', {
+    new CfnOutput(this, 'VPCid', {
       value: vpc.vpcId,
       description: "VPC id",
       exportName: "vpc-id"
