1ee3a55d64b58bfa25ab93f05f4e2435449a25e4
Jyrki Ahpola <jahpola@users.noreply.github.com>
2026-03-29 16:46:41 +0300

    Update CDK etc (#1)
    * Update libs
    * Update license

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,20 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import * as path from 'path';
-import { Vpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as rds from 'aws-cdk-lib/aws-rds';
+import * as path from "path";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 import { Construct } from "constructs";
-import {
-  CfnOutput,
-  Duration,
-  Stack,
-  StackProps,
-  RemovalPolicy,
-} from 'aws-cdk-lib';
 
-export class LambdaRdsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class LambdaRdsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
@@ -23,86 +16,85 @@ export class LambdaRdsStack extends Stack {
     // ISOLATED: Isolated Subnets do not route traffic to the Internet (in this VPC).
     // PRIVATE.: Subnet that routes to the internet using nat gateway(s), but not vice versa.
     // PUBLIC..: Subnet connected to the Internet
-    const vpc = new Vpc(this, 'network', {
+    const vpc = new ec2.Vpc(this, "network", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
       natGateways: 1, // In production this should be 3, remember AZ design in AWS ie design for loss of 1 az always
       subnetConfiguration: [
         {
-          name: 'public', subnetType: SubnetType.PUBLIC,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC,
         },
         {
-          name: 'private', subnetType: SubnetType.PRIVATE_WITH_EGRESS
+          name: "private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
         },
         {
-          name: 'db-net', subnetType: SubnetType.PRIVATE_ISOLATED
-        }
-      ]
+          name: "db-net",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
     });
 
     // Manually creating security group => needs sg, ingress,egress rules etc created manually
     // Instead we are using .connection system, see below for example .connection use
 
     // If we want to rds to use isolated subnets we need to some manual stuff
-    const db_subnet = new rds.SubnetGroup(this, 'db-subnet', {
-      description: 'Isolated DB net',
+    const db_subnet = new rds.SubnetGroup(this, "db-subnet", {
+      description: "Isolated DB net",
       vpc: vpc,
-      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED }
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
     });
 
-    const db = new rds.ServerlessCluster(this, 'db', {
+    const db = new rds.ServerlessCluster(this, "db", {
       engine: rds.DatabaseClusterEngine.AURORA_MYSQL,
-      defaultDatabaseName: 'serverless',
+      defaultDatabaseName: "serverless",
       vpc: vpc,
       scaling: {
-        autoPause: Duration.minutes(10),
+        autoPause: cdk.Duration.minutes(10),
         minCapacity: 1,
-        maxCapacity: 4
+        maxCapacity: 4,
       },
       credentials: rds.Credentials.fromGeneratedSecret("serverless"),
       deletionProtection: false,
-      removalPolicy: RemovalPolicy.DESTROY, // Just delete the db, this is hazard!
-      // default ie no subnetgroup = private_with_egress automatically selected
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Just delete the db, this is hazard!
+      // default ie no subnetgroup = private_with_nat automatically selected
       // note that changing these values will destroy the old db and create new one
-      subnetGroup: db_subnet
+      subnetGroup: db_subnet,
     });
 
-    // Yes, the lambda code is utter crap... should be nuked from orbit and rewritten 
-    const reader = new NodejsFunction(this, 'reader-function', {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: 'main',
-      entry: path.join(__dirname, '../src/db-reader/main.ts'),
+    // Yes, the lambda code is utter crap... should be nuked from orbit and rewritten
+    const reader = new NodejsFunction(this, "reader-function", {
+      runtime: lambda.Runtime.NODEJS_24_X,
+      handler: "main",
+      entry: path.join(__dirname, "../src/db-reader/main.ts"),
       tracing: lambda.Tracing.ACTIVE,
       bundling: {
-        externalModules: ['aws-sdk'],
+        externalModules: ["@aws-sdk/*"],
       },
       environment: {
-        HOST: db.clusterEndpoint.hostname,
-        // unsafeUnwrap is used to get the value from the secret, not recommended in production
-        DBNAME: db.secret?.secretValueFromJson('dbname').unsafeUnwrap()!,
-        USER: db.secret?.secretValueFromJson('username').unsafeUnwrap()!,
-        SECRET: db.secret?.secretValueFromJson('password').unsafeUnwrap()!
+        SECRET_ARN: db.secret!.secretArn,
       },
       vpc: vpc,
       vpcSubnets: {
         // PRIVATE_WITH_EGRESS is required with outside access usually
-        // ISOLATION might require private link+api etc endpoints, 
-        // recommendation: just use private_with_egress unless working in high security environments
-        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        // ISOLATION might require private link+api etc endpoints,
+        // recommendation: just use PRIVATE_WITH_EGRESS unless working in high security environments
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
       },
       architecture: lambda.Architecture.ARM_64,
     });
 
-    // Allow access from reader lambda without creating a sg..
-    db.connections.allowDefaultPortFrom(reader)
+    db.secret!.grantRead(reader);
+    db.connections.allowDefaultPortFrom(reader);
 
     // Allows DataAPI access
     //db.grantDataApiAccess(reader)
 
     // Outputs for import/export across stacks as needed below
-    new CfnOutput(this, 'VPCid', {
+    new cdk.CfnOutput(this, "VPCid", {
       value: vpc.vpcId,
       description: "VPC id",
-      exportName: "vpc-id"
+      exportName: "vpc-id",
     });
-
   }
 }
--- test/lambda-rds.test.ts
+++ test/lambda-rds.test.ts
@@ -1,12 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import * as LambdaRds from '../lib/infra-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as LambdaRds from "../lib/infra-stack";
 
-test('RDS exists', () =>{
-    const app = new cdk.App();
+test("RDS exists", () => {
+  const app = new cdk.App();
 
-    const stack = new LambdaRds.LambdaRdsStack(app, 'MyTestStack');
-    const template = Template.fromStack(stack);
+  const stack = new LambdaRds.LambdaRdsStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
 
-    template.resourceCountIs("AWS::RDS::DBCluster",1);
+  template.resourceCountIs("AWS::RDS::DBCluster", 1);
 });
